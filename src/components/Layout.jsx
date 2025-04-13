6fa1d047375c6d38ff65ed3aec0cfe1641eea224
Wajahat Iqbal <wajahat.iqbal@laam.pk>
2025-04-14 01:53:17 +0500

    basic app

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
-import { Link } from 'react-router-dom'; 
+import { Link, useNavigate } from 'react-router-dom';
 
 
 const Header = () => {
+    const navigate = useNavigate();
+    const handleLogout = () => {
+        localStorage.removeItem('token');
+        navigate('/');
+      };
   return (
     <header className="bg-indigo-600 text-white py-4 shadow-md sticky top-0 z-10">
       <div className="max-w-7xl mx-auto px-6 flex justify-between items-center">
@@ -12,15 +17,25 @@ const Header = () => {
         <nav>
           <ul className="flex space-x-6">
             <li>
-              <Link to="/" className="hover:text-indigo-300">Home</Link>
+              <Link to="/products" className="hover:text-indigo-300">Products</Link>
+            </li>
+            <li>
+              <Link to="/categories" className="hover:text-indigo-300">Categories</Link>
             </li>
             <li>
               <Link to="/about" className="hover:text-indigo-300">About</Link>
             </li>
             <li>
-              <Link to="/contact" className="hover:text-indigo-300">Contact</Link>
-            </li>
+            <button
+            onClick={handleLogout}
+            className="bg-white text-indigo-600 px-4 py-1 rounded hover:bg-gray-100 transition"
+          >
+            Logout
+          </button>
+                </li>
+
           </ul>
+          
         </nav>
       </div>
     </header>
--- src/routes.jsx
+++ src/routes.jsx
@@ -1,24 +1,31 @@
 import { createBrowserRouter } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
 import HomePage from './pages/HomePage';
-import Layout from "./components/Layout"
+import Layout from './components/Layout';
+import PrivateRoute from './PrivateRoute'
 
 const routes = createBrowserRouter([
-  {
-    path: '/',
-    element: <Layout />, // Use the Layout component here
-    children: [
-      {
-        path: '/',
-        element: <LoginPage />, // Home page content
-      },
-      {
-        path: '/home',
-        element: <HomePage />, // Login page content
-      },
-      // Add more routes as needed
-    ],
-  },
-]);
-
-export default routes;
+    {
+      path: '/',
+      element: <LoginPage />, // Only login page has no layout
+    },
+    {
+      path: '/',
+      element: <PrivateRoute />, // check if user is logged in
+      children: [
+        {
+          path: '/',
+          element: <Layout />, // Layout (Header/Footer)
+          children: [
+            {
+              path: 'home',
+              element: <HomePage />,
+            },
+            // Add more routes here later
+          ],
+        },
+      ],
+    },
+  ]);
+  
+  export default routes;
